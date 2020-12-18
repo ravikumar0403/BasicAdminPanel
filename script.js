df73ad5157a91bd416b6544ba1b568aa50a58391
Ravi Kumar <kumarravi431999@gmail.com>
2020-12-18 19:44:51 +0530

    bug fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const url = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
 
 var userDetails = []
-$.get(url, data => {
-    userDetails = data.slice(0,5)
-    createDataRows(userDetails)
+$.get(url, function( data,status ) {
+    if(status==="success"){
+        userDetails = data.slice(0,5);
+        createDataRows(userDetails)
+    }
 })
 
 function createDataRows(data){
